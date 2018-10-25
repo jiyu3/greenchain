0dba038bdf334b4d6d47c541db85b2b546416297
jiyu <m@jiyu.lol>
2018-10-25 13:03:44 +0900

    add google analytics

--- client/src/App.vue
+++ client/src/App.vue
@@ -104,6 +104,38 @@ export default {
 			ad.style.display = this.$route.name === "Sponsor" ? "block" : "none"
 		}
 	},
+	created() {
+		(function (i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function () {
+				(i[r].q = i[r].q || []).push (arguments)
+			}, i[r].l = 1 * new Date ();
+			a = s.createElement (o), m = s.getElementsByTagName (o)[0];
+			a.async = 1;
+			a.src   = g;
+			m.parentNode.insertBefore (a, m)
+		}) (window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+		ga ('create', 'UA-123676187-1', 'auto');
+		ga ('send', 'pageview');
+
+		function insertCallback(parent, funcname, callback, ...args) {
+			let oldFunc = parent[funcname] ? parent[funcname] : function (){}
+			parent[funcname] = function() {
+				oldFunc.apply(this, arguments)
+				return callback(...args)
+			}
+		}
+
+		function notify_analytics(l) {
+			let newPage = l.pathname + l.hash
+			ga('set', 'page', newPage)
+			ga('send', 'pageview')
+		}
+
+		insertCallback(window.history, "pushState", notify_analytics, location)
+		insertCallback(window.history, "replaceState", notify_analytics, location)
+	},
 	mounted() {
 		this.checkAd()
 	}
