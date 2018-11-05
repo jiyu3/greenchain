1f27ab910d14dee125289048a1e18e31007e7623
jiyu <m@jiyu.lol>
2018-11-05 17:12:06 +0900

    testnet version

--- client/src/components/BlockLatest/index.vue
+++ client/src/components/BlockLatest/index.vue
@@ -13,137 +13,55 @@ export default {
 				payreq: null,
 				node: null
 			},
-			block: block
-// 			visible: [true],
-// 			nb_block: block,
-// 			event: null,
-// 			img: [
-// 				{
-// 					id: "page_0",
-// //					src: null,
-// 					src: null,
-// 					next: "#page_1"
-// 				}
-// 			],
-// 			prev: `/block/${block - 1}`,
-// 			next: `/block/${block + 1}`
+			nb_block: block,
+			prev: `/block/${block - 1}`,
+			next: `/block/${block + 1}`,
+			img: [],
+			img_loaded: false
 		}
 	},
 	methods: {
-		/*
-			localStorage で値作成
-			POST /invoice でインボイス作成
-			POST /invoice/:id/webhook で localStorage 値つきのwebhook作成
+		init() {
+			if(this.$route.params.id == "3") {
+				if( this.$i18n.locale != "ja" &&
+					this.$i18n.locale != "zh-cn" &&
+					this.$i18n.locale != "zh-tw") {
+					this.$i18n.locale = "ja"
+				}
+			}
 
-			webhookが叩かれたら、DBにlocalStorageでpaidフラグを立てる
+			this.rpc("cln", "pay", { msatoshi: 2 }, false).then(r => {
+				QRCode.toDataURL(r.invoice.payreq, (err, qr) => {
+					this.qr.payreq = qr
+				})
+				QRCode.toDataURL(r.node, (err, qr) => {
+					this.qr.node = qr
+				})
 
-			1秒ごとにDBを読み込みに行き、localStorageにpaidフラグが立っていたら画像を規定回数読み込む
-		*/
-// 		handleScroll(nb_page) {
-// 			let elem = this.$refs[`page_${nb_page}`][0]
-// 			if(elem === void 0) {
-// 				return false;
-// 			}
-// 			if(!this.visible[nb_page]) {
-// 				let top = elem.getBoundingClientRect().top
-// 				this.visible[nb_page] = top < window.innerHeight + 100
-// 			}
-// 			if(this.visible[nb_page]) {
-// 				window.removeEventListener('scroll', this.event)
-
-// 				let src = null
-// 				try {
-// 					src = require(`../../images/blocks/${this.nb_block}/${this.$i18n.locale}/${nb_page+1}.jpg`)
-// 				} catch(e) {
-// 					document.getElementById(`page_${nb_page}`).href = "#footer"
-// 					return false
-// 				}
-
-// 				this.img.push({
-// 					id: `page_${nb_page+1}`,
-// //					src: 'data:image.png;base64,' + r.img,
-// 					src: src,
-// 					next: `#page_${nb_page+2}`
-// 				})
-
-// 				this.event = () => {
-// 					this.handleScroll(nb_page+1)
-// 				}
-// 				window.addEventListener('scroll', this.event)
-
-
-				// this.rpc("block", "read", { nb_block: this.nb_block, nb_page: nb_page+5, lang: this.$i18n.locale }, false).then(r => {
-				// 	console.log("loaded: " + `page_${nb_page+5}`)
-				// 	this.img.push({
-				// 		id: `page_${nb_page+5}`,
-				// 		src: 'data:image.png;base64,' + r.img,
-				// 		next: `#page_${nb_page+6}`
-				// 	})
-
-				// 	this.event = () => {
-				// 		this.handleScroll(nb_page+1)
-				// 	}
-				// 	window.addEventListener('scroll', this.event)
-				// }).catch(e => {
-				// 	document.getElementById(`page_${nb_page}`).href = "#footer"
-				// })
-		// 	}
-		// },
-		// init() {
-		// 	this.loaded = false
-
-		// 	this.img = []
-		// 	this.img = [{
-		// 		id: `page_0`,
-		// 		src: require(`../../images/blocks/${this.nb_block}/${this.$i18n.locale}/0.jpg`),
-		// 		next: `#page_1`
-		// 	}]
-
-		// 	this.loaded = true
-
-		// 	if(this.event != null) {
-		// 		window.removeEventListener('scroll', this.event)
-		// 	}
-		// 	this.event = () => {
-		// 		this.handleScroll(0)
-		// 	}
-		// 	window.addEventListener('scroll', this.event)
-		// }
+				setTimeout(() => {
+					this.rpc("cln", "if_pay_then_read", { id: r.invoice.id, timeout: 11 * 60 * 1000, block: this.nb_block, lang: this.$i18n.locale }, false).then(r => {
+						r.img.forEach((e, i) => {
+							this.img.push({
+								id: `page_${i+1}`,
+								src: 'data:image/jpeg;base64,' + r.img[i],
+								next: `#page_${i+2}`
+							})
+						})
+						this.img_loaded = true
+					})
+				}, 0)
+			}).catch(e => {
+				console.log(e)
+			})
+		}
 	},
 	watch: {
-		// "$i18n.locale"(v) {
-		// 	this.init()
-		// },
-		// "$route.params.id"(v) {
-		// 	window.scrollTo(0, 0)
-		// 	v = +v
-		// 	this.nb_block = v
-		// 	this.prev = `/block/${v - 1}`
-		// 	this.next = `/block/${v + 1}`
-		// 	this.init()
-		// }
+		"$i18n.locale"(v) {
+			this.init()
+		},
 	},
 	created() {
-		this.rpc("cln", "get_node_info", { msatoshi: 137000 }, false).then(r => {
-			console.log(r.invoice)
-
-			QRCode.toDataURL(r.invoice.payreq, (err, qr) => {
-				this.qr.payreq = qr
-			})
-			QRCode.toDataURL(r.node, (err, qr) => {
-				this.qr.node = qr
-			})
-
-			this.rpc("cln", "if_pay_then_read", { id: "OHOYYjV4kj0ItuwVgvQ22", timeout: 11 * 60 * 1000, block: this.block, lang: this.$i18n.locale }, true).then(r => {
-				console.log(r)
-			})
-		}).catch(e => {
-			console.log(e)
-		})
-
-//		this.init()
+		this.init()
 	},
-	mounted() {
-	}
 }
 </script>
--- server/routes/cln.js
+++ server/routes/cln.js
@@ -12,6 +12,9 @@ const DB = new db()
 let cln = require("../controllers/cln.class")
 const CLN = new cln()
 
+let util = require("../lib/util.class")
+const UTIL = new util()
+
 let CHARGE = `http://api-token:midori@test.cln.green:9112`
 let NODE = `031201e62297a420a3878d0d8b7c4206553d354097da1a9e7c34158303d9223569@testnet.cln.green:9735`
 
@@ -21,10 +24,14 @@ router.get('/', function (req, res, next) {
 	res.send('Be yourself; everything else is taken.')
 })
 
-router.post('/get_node_info', async (req, res, next) => {
+let fee = {
+	manga: 2
+}
+
+router.post('/pay', async (req, res, next) => {
 	let p = req.body.params
 
-	request.post(`${CHARGE}/invoice`, {form: {msatoshi: p.msatoshi}}, (err, resp, body) => {
+	request.post(`${CHARGE}/invoice`, { form: { msatoshi: p.msatoshi } }, (err, resp, body) => {
 		if (err) {
 			res_rpc.result = { error: err }
 		} else {
@@ -39,26 +46,37 @@ router.post('/if_pay_then_read', async (req, res, next) => {
 	let fs = require("fs")
 	let url = `${CHARGE}/invoice/${p.id}/wait?timeout=${p.timeout}`
 
-	console.log("if_pay_then_read")
 	request.get(url, (err, resp, body) => {
-		console.log(`${p.id} has been paid!`)
+		console.log(body)
+		body = JSON.parse(body)
+		if (body.msatoshi_received < fee.manga) {
+			res_rpc.result = Object.assign({ error: "insufficient fee" }, { img: null })
+			res.json(JSON.stringify(res_rpc))
+			return false
+		}
 
 		let i = 0
 		let images = []
+		let promises = []
 		while (1) {
-			let imgPath = __dirname + `/../blocks/${p.block}/${p.lang}/${i++}.jpg`
+			let imgPath = __dirname + `/../blocks/${p.block}/${p.lang}/${i}.jpg`
 			if (fs.existsSync(imgPath)) {
-				fs.readFile(imgPath, "base64", (err, img) => {
-					images[i] = img
-				})
+				let index = i.toString()
+				promises.push(
+					UTIL.readFileAsync(imgPath).then(img => {
+						images[index] = img
+					})
+				)
+				i++
 			} else {
-				console.log(`${imgPath} does not exist`)
 				break;
 			}
 		}
-		res_rpc.result = Object.assign({ error: null }, { img: images })
-		console.log("images", images)
-		res.json(JSON.stringify(res_rpc))
+
+		Promise.all(promises).then(() => {
+			res_rpc.result = Object.assign({ error: null }, { img: images })
+			res.json(JSON.stringify(res_rpc))
+		})
 	})
 })
 
